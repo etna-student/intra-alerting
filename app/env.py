a6a0d82c0a61b8e4d13b1fc7122e3db38886198d
Mathurin Chung <153829122+etna-student@users.noreply.github.com>
2024-10-15 23:35:54 +0200

    . .env.example .env app/google_chat.py | update environment variables

--- app/env.py
+++ app/env.py
@@ -5,7 +5,7 @@ def get_dotenv():
     load_dotenv()
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
-    google_chat_webhook_url = os.getenv('GOOGLE_CHAT_WEBHOOK_URL')
+    google_chat_webhook_informations = os.getenv('GOOGLE_CHAT_WEBHOOK_INFORMATIONS')
 
-    return { 'login': login, 'password': password, 'google_chat_webhook_url': google_chat_webhook_url }
+    return { 'login': login, 'password': password, 'google_chat_webhook_informations': google_chat_webhook_informations }
 
--- app/google_chat.py
+++ app/google_chat.py
@@ -3,7 +3,7 @@ from env import get_dotenv
 
 def send_notification_to_google_chat(message):
     dotenv = get_dotenv()
-    url = dotenv['google_chat_webhook_url']
+    url = dotenv['google_chat_webhook_informations']
 
     data = { 'text': message }
     requests.post(url, json=data)
